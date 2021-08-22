4360c1ef81380b16d653d55f7185ba02950f6e11
ryotagonG <ryotapb715@gmail.com>
2021-08-22 08:08:10 +0000

    updated

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'caf1ad2276163b5c3602ee8308ad0434460365f78832a30a114d8ebbda81d59783050c48867798f20189ed7ff494fc8faaae5014e97cc250bd323a96a54ffb5a'
