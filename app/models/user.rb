4c101a39d0ce3e6d3682888d230e4607925333b1
ryotagonG <ryotapb715@gmail.com>
2021-08-28 07:42:53 +0000

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
-  validates :name, presence: true,
+  validates :name, presence: true, uniqueness: true,
     length: { minimum: 2, maximum: 20 }
   validates :introduction,
     length: { maximum: 50 }
